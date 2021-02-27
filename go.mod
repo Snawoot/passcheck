491835230ebc6895712701d35ca81f5a5e3d57c2
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-02-28 00:04:09 +0200

    add initial code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Snawoot/passcheck
 
 go 1.15
+
+require github.com/mattevans/pwned-passwords v0.3.0
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"log"
+	"encoding/csv"
+	"io"
+
+	hibp "github.com/mattevans/pwned-passwords"
+)
+
+const (
+	storeExpiry = 1 * time.Hour
+)
+
+func main() {
+	client := hibp.NewClient(storeExpiry)
+
+	log.Println("Reading CSV file with login,password pairs from STDIN...")
+
+	r := csv.NewReader(os.Stdin)
+	r.FieldsPerRecord = 2
+	r.LazyQuotes = true
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		login, password := record[0], record[1]
+
+		pwned, err := client.Pwned.Compromised(password)
+		if err != nil {
+			log.Printf("hibp client error: %v. Login %s wasn't processed.", err, login)
+			continue
+		}
+
+		if pwned {
+			fmt.Println(login)
+			log.Printf("Found breach for login \"%s\"!", login)
+		}
+	}
+
+}
