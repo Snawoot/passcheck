5e720b17782dd0aba0f5d3e593711bb4cc86bd20
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-02-28 02:04:14 +0200

    escape all CSV fields

--- scan-passwordstore.sh
+++ scan-passwordstore.sh
@@ -11,5 +11,5 @@ PASSCHECK=${PASSCHECK:-$bin}
 while read -r fn ; do
 	n="${fn#$PASS_DIR}"
 	n="${n%.gpg}"
-	p="$(gpg -d "$fn" 2>/dev/null)" && echo "$n,\"${p//\"/\"\"}\""
-done < <(find "$PASS_DIR" -type f -name \*.gpg) | "$PASSCHECK"
+	p="$(gpg -d "$fn" 2>/dev/null)" && echo "\"${n//\"/\"\"}\",\"${p//\"/\"\"}\""
+done < <(find "$PASS_DIR" -type f -name \*.gpg) | "$PASSCHECK" >/dev/null
